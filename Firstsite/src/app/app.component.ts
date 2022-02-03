ab6b4b76b9de0ce191c4faedeb88a04713ba73f9
lilyan <lilyan.bastien@gmail.com>
2022-02-03 23:50:03 +0100

    pipe

--- Firstsite/src/app/app.component.ts
+++ Firstsite/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit{
         title: "pierre",
         description: "c'est une personne",
         date: new Date(),
-        like: 7,
+        like: 40,
         imageURL: "https://img-19.ccm2.net/iBYO1DOif2mcoMT7crnZ0Yy3XaU=/480x270/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg",
         location: 'Paris'
       },
@@ -23,7 +23,7 @@ export class AppComponent implements OnInit{
         title: "jean",
         description: "c'est une deuxième personne",
         date: new Date(),
-        like: 863,
+        like: 840,
         imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Himalayas.jpg/330px-Himalayas.jpg",
         location: 'la montagne'
       },
@@ -31,7 +31,7 @@ export class AppComponent implements OnInit{
         title: "nicolas",
         description: "c'est une 3ème personne",
         date: new Date(),
-        like: 75,
+        like: 130,
         imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Alpamayo.jpg/330px-Alpamayo.jpg",
       }
 
